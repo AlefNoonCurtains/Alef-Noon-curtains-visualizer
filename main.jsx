b857722550f8e429abc7a831b6e10bb9945ef80c
AlefNoonCurtains <alefnooncurtains@gmail.com>
2025-08-03 17:41:55 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CurtainVisualizerCloudinary from './App';
+import './App.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<CurtainVisualizerCloudinary />);
